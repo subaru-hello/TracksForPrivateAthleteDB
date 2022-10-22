54ef01d5157af89237d217d211f850a232bc48ef
subaru-hello <octosubaru0926@gmail.com>
2022-10-22 09:30:27 +0900

    theme

--- src/Providers.tsx
+++ src/Providers.tsx
@@ -2,10 +2,21 @@ import type { FC, PropsWithChildren } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
 import { HelmetProvider } from 'react-helmet-async';
+import { extendTheme } from '@chakra-ui/react';
+
+const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+};
+
+const theme = extendTheme({ colors });
 
 const Providers: FC<PropsWithChildren> = ({ children }) => (
   <HelmetProvider>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Router>{children}</Router>
     </ChakraProvider>
   </HelmetProvider>
--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { extendTheme } from '@chakra-ui/react';
 
-const colors = {
-  brand: {
-    900: '#1a365d',
-    800: '#153e75',
-    700: '#2a69ac',
-  },
-};
-
-const theme = extendTheme({ colors });
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
