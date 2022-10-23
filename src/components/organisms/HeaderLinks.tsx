f1e9fa304a27a1defe0e0b5671e02a57d4854331
subaru-hello <octosubaru0926@gmail.com>
2022-10-23 15:34:32 +0900

    remove find track logic

--- src/components/organisms/HeaderLinks.tsx
+++ src/components/organisms/HeaderLinks.tsx
@@ -16,29 +16,11 @@ import { HamburgerIcon, Search2Icon } from '@chakra-ui/icons';
 import { trackData } from 'data';
 import { Track } from 'domains';
 
-type Props = { lists: string[], tracks: Track[] };
+type Props = { lists: string[] };
 
 const HeaderLinks: FC<Props> = (props) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const handleToggle = () => (isOpen ? onClose() : onOpen());
-  // 競技場データを配列に保持する
-  const tracks = trackData;
-
-  // ユーザーの入力キーワードをState化する
-  const [searchKeyword, updateSearchKeyword] = useState('');
-
-  // 入力イベントに反応してStateを更新する
-  const onInput = (event: React.FormEvent<HTMLInputElement>) => {
-    // 入力キーワードをstateに格納する
-    updateSearchKeyword(event.currentTarget.value);
-  };
-
-  const filteredList = tracks.filter((item) => {
-    // ユーザー入力を安全に正規表現にする（このときすべて小文字化で正規化する）
-    const escapedText = escapeStringRegexp(searchKeyword.toLowerCase());
-    // 小文字で比較して部分一致するものだけを残す
-    return new RegExp(escapedText).test(item.name.toLowerCase());
-  });
 
   return (
     <Flex
@@ -53,7 +35,7 @@ const HeaderLinks: FC<Props> = (props) => {
     >
       <Flex align="center" mr={5}>
         <Heading as="h1" size="lg" letterSpacing={'tighter'}>
-          競技場検索
+          <Link to="/">競技場検索</Link>
         </Heading>
       </Flex>
 
@@ -76,17 +58,8 @@ const HeaderLinks: FC<Props> = (props) => {
           htmlSize={30}
           width="auto"
           type="text"
-          onInput={onInput}
           placeholder="競技場名を入れて下さい"
         />
-        
-          {filteredList.map((track) => {
-            return track && <ul key={track.id} className="list">
-                <li >{track.name}</li>
-                <li >{track.site_url}</li>
-                <li >{track.entrance_fee}</li>
-        </ul>
-          })}
       </Stack>
 
       <Box
