26fdcbfbcd861f3d66095922c04843a681d46c85
subaru-hello <octosubaru0926@gmail.com>
2022-10-25 08:21:19 +0900

    user schema

---
+++ src/data/users.ts
@@ -0,0 +1,29 @@
+
+import { User } from "domains";
+
+export const prefectureData: User[] = [
+    {
+        id: 1,
+        uuid: '123456',
+        name: 'たこ',
+        email: 'tako@example.com',
+    },
+    {
+        id: 2,
+        uuid: '123456',
+        name: 'いか',
+        email: 'ika@example.com',
+    },
+    {
+        id: 3,
+        uuid: '123456',
+        name: 'えび',
+        email: 'ebi@example.com',
+    },
+]
+// export interface User {
+//     id: string;
+//     uuid: number;
+//     name: string;
+//     email?: string;
+//   }
--- src/domains/types.ts
+++ src/domains/types.ts
@@ -9,9 +9,24 @@ export interface Prefecture {
 export interface Track {
     id: string;
     name: string;
+    furigana: string
     prefectureID: typeof PREFECTURE_CODE[number];
     address?: string;
     open_hour: string;
     entrance_fee: number;
     site_url: string;
+  }
+
+export interface User {
+    id: number;
+    uuid: string;
+    name: string;
+    email?: string;
+  }
+
+export interface Comment {
+    id: number;
+    title: string;
+    body: string; 
+    userID: number;
   }
