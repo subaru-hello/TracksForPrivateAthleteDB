6f7c3e146ae346085cda47db923b8918c195cece
subaru-hello <octosubaru0926@gmail.com>
2022-10-25 08:22:18 +0900

    remove track data from header

--- src/components/templates/Header.tsx
+++ src/components/templates/Header.tsx
@@ -4,12 +4,10 @@ import { trackData } from 'data';
 import  HeaderLinks  from 'components/organisms/HeaderLinks';
 
 const Header: FC = () => {
-const tracks = trackData;
-const lists = ['']
 
     return(
       <Box>
-        <HeaderLinks lists={lists} tracks={tracks}/>
+        <HeaderLinks />
       </Box>
     )
 }
