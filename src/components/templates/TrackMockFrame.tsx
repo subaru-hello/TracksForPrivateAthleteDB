3a1b36a0e5e095b6379e164f7084ceb197f6045e
subaru-hello <octosubaru0926@gmail.com>
2022-11-02 21:57:38 +0900

    add mock track crd

---
+++ src/components/templates/TrackMockFrame.tsx
@@ -0,0 +1,20 @@
+import type { FC } from 'react';
+import { Outlet } from 'react-router-dom';
+import { Box, Heading, Container } from '@chakra-ui/react';
+import HomeButton from 'components/organisms/HomeButton';
+
+const TrackMockFrame: FC = () => (
+  <Container>
+    <header>
+      <Heading as="h1" size="xl" my={4}>
+        競技場編集
+      </Heading>
+    </header>
+    <Outlet />
+    {/* 検索フォームテンプレート */}
+
+    <HomeButton my={16} />
+  </Container>
+);
+
+export default TrackMockFrame;
