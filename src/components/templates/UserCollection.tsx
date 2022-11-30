7b1d4625bbedacdd78b36ad7720e03ed387c30b6
subaru-hello <octosubaru0926@gmail.com>
2022-11-30 23:25:15 +0900

    add firebase crud about users and associated comments

---
+++ src/components/templates/UserCollection.tsx
@@ -0,0 +1,136 @@
+import { useState, useEffect, FC } from 'react';
+import {
+  collection,
+  onSnapshot,
+  addDoc,
+  serverTimestamp,
+  deleteDoc,
+  doc,
+  updateDoc,
+} from 'firebase/firestore';
+import { db } from 'Firebase';
+import { Link, Outlet } from 'react-router-dom';
+import {
+  List,
+  ListItem,
+  Box,
+  Text,
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+} from '@chakra-ui/react';
+
+import UserCommentCollection from 'components/templates/UserCommentCollection';
+
+type FirebaseProps = {
+  id: string;
+  name: string;
+  email: string;
+  admin?: boolean;
+};
+const UserCollection: FC = () => {
+  const [users, setUsers] = useState<FirebaseProps[]>([]);
+
+  useEffect(() => {
+    // usersコレクションを参照する
+    const usersCollectionRef = collection(db, 'users');
+    // 変更をリアルタイムで検知してusersを変更する
+    const unsub = onSnapshot(usersCollectionRef, (querySnapshot) => {
+      setUsers(
+        querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+      );
+    });
+    return unsub;
+  }, []);
+
+  const handleSubmit = async (event: {
+    preventDefault: () => void;
+    target: { elements: { name: any; email: any } };
+  }) => {
+    event.preventDefault();
+    const { name, email } = event.target.elements;
+    const usersCollectionRef = collection(db, 'users');
+    const documentRef = await addDoc(usersCollectionRef, {
+      name: name.value,
+      email: email.value,
+      timpstamp: serverTimestamp(),
+    });
+    console.log(documentRef);
+  };
+
+  const deleteUser = async (id: string) => {
+    const userDocumentRef = doc(db, 'users', id);
+    await deleteDoc(userDocumentRef);
+  };
+
+  const changeAdmin = async (id: string) => {
+    const userDocumentRef = doc(db, 'users', id);
+    await updateDoc(userDocumentRef, {
+      admin: true,
+    });
+  };
+
+  return (
+    <Box>
+      <TableContainer>
+        <Table variant="simple">
+          <TableCaption>ユーザー情報</TableCaption>
+          <Thead>
+            <Tr>
+              <Th>ID</Th>
+              <Th>名前</Th>
+              <Th>メールアドレス</Th>
+            </Tr>
+          </Thead>
+          {users.map((user) => (
+            <Tbody key={user.id}>
+              <Tr>
+                <Td>{user.id}</Td>
+                <Td>
+                  <Link to={user.id}>{user.name}</Link>
+                </Td>
+                <Td>{user.email}</Td>
+                {user.admin && (
+                  <Td onClick={() => deleteUser(user.id)}>削除</Td>
+                )}
+              </Tr>
+            </Tbody>
+          ))}
+          <Tfoot>
+            <Tr>
+              <Th>ID</Th>
+              <Th>name</Th>
+              <Th>email</Th>
+            </Tr>
+          </Tfoot>
+        </Table>
+      </TableContainer>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>名前</label>
+          <input name="name" type="text" placeholder="名前" />
+        </div>
+        <div>
+          <label>メールアドレス</label>
+          <input name="email" type="email" placeholder="メールアドレス" />
+        </div>
+        <div>
+          <button>登録</button>
+        </div>
+      </form>
+      <div>
+        {users.map((user) => (
+          <UserCommentCollection id={user.id} />
+        ))}
+      </div>
+    </Box>
+  );
+};
+
+export default UserCollection;
---
+++ src/components/templates/UserCommentCollection.tsx
@@ -0,0 +1,130 @@
+import { useState, useEffect, FC } from 'react';
+import {
+  collection,
+  onSnapshot,
+  addDoc,
+  serverTimestamp,
+  deleteDoc,
+  doc,
+  updateDoc,
+} from 'firebase/firestore';
+import { db } from 'Firebase';
+import { Link, Outlet } from 'react-router-dom';
+import {
+  List,
+  ListItem,
+  Box,
+  Text,
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+} from '@chakra-ui/react';
+
+type FirebaseCommentProps = {
+  id: string;
+  title: string;
+  content: string;
+};
+
+type UserCollectionProps = {
+  id?: string;
+};
+
+const UserCommentCollection: FC<UserCollectionProps> = ({ id = '' }) => {
+  const [comments, setComments] = useState<FirebaseCommentProps[]>([]);
+  console.log(id);
+  useEffect(() => {
+    // usersコレクションを参照する
+    const usersCollectionRef = collection(db, 'users', id, 'comments');
+    // 変更をリアルタイムで検知してusersを変更する
+    const unsub = onSnapshot(usersCollectionRef, (querySnapshot) => {
+      setComments(
+        querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+      );
+    });
+    return unsub;
+  }, []);
+
+  const handleSubmitComment = async (event: {
+    preventDefault: () => void;
+    target: { elements: { title: any; content: any } };
+  }) => {
+    event.preventDefault();
+    const { title, content } = event.target.elements;
+    const usersCollectionRef = collection(db, 'users', id, 'comments');
+    const documentRef = await addDoc(usersCollectionRef, {
+      title: title.value,
+      content: content.value,
+      timpstamp: serverTimestamp(),
+    });
+    console.log(documentRef);
+  };
+
+  const deleteUser = async (id: string) => {
+    const userDocumentRef = doc(db, 'users', id);
+    await deleteDoc(userDocumentRef);
+  };
+
+  const changeAdmin = async (id: string) => {
+    const userDocumentRef = doc(db, 'users', id);
+    await updateDoc(userDocumentRef, {
+      admin: true,
+    });
+  };
+
+  return (
+    <Box>
+      <TableContainer>
+        <Table variant="simple">
+          <TableCaption>コメント情報</TableCaption>
+          <Thead>
+            <Tr>
+              <Th>ID</Th>
+              <Th>タイトル</Th>
+              <Th>内容</Th>
+            </Tr>
+          </Thead>
+          {comments.map((comment) => (
+            <Tbody key={comment.id}>
+              <Tr>
+                <Td>{comment.id}</Td>
+                <Td>
+                  <Link to={comment.id}>{comment.title}</Link>
+                </Td>
+                <Td>{comment.content}</Td>
+              </Tr>
+            </Tbody>
+          ))}
+          <Tfoot>
+            <Tr>
+              <Th>ID</Th>
+              <Th>タイトル</Th>
+              <Th>内容</Th>
+            </Tr>
+          </Tfoot>
+        </Table>
+      </TableContainer>
+      <form onSubmit={handleSubmitComment}>
+        <div>
+          <label>タイトル</label>
+          <input name="title" type="text" placeholder="title" />
+        </div>
+        <div>
+          <label>コンテンツ</label>
+          <input name="content" type="text" placeholder="content" />
+        </div>
+        <div>
+          <button>登録</button>
+        </div>
+      </form>
+    </Box>
+  );
+};
+
+export default UserCommentCollection;
