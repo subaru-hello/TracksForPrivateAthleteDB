15d5f6220906e3005c686cfd9289f47bb60a4d61
subaru-hello <octosubaru0926@gmail.com>
2022-12-05 08:47:27 +0900

    templates/tracks

--- src/components/templates/tracks/TracksFrame.tsx
+++ src/components/templates/tracks/TracksFrame.tsx
@@ -1,20 +1,18 @@
 import type { FC } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Box, Heading, Container } from '@chakra-ui/react';
-import HomeButton from 'components/organisms/HomeButton';
+import HomeButton from 'components/organisms/layouts/HomeButton';
 
-const TrackMockFrame: FC = () => (
+const TracksFrame: FC = () => (
   <Container>
     <header>
       <Heading as="h1" size="xl" my={4}>
-        競技場編集
+        全国の競技場
       </Heading>
     </header>
     <Outlet />
-    {/* 検索フォームテンプレート */}
-
     <HomeButton my={16} />
   </Container>
 );
 
-export default TrackMockFrame;
+export default TracksFrame;
