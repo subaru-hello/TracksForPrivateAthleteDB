34170ef831d9bd7294270e2a26c97d0a4c6b1b9d
subaru-hello <octosubaru0926@gmail.com>
2022-12-04 23:16:38 +0900

    add AuthProvider to have global state of currentUser

--- src/Providers.tsx
+++ src/Providers.tsx
@@ -2,13 +2,15 @@ import type { FC, PropsWithChildren } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
 import { HelmetProvider } from 'react-helmet-async';
-
+import { AuthProvider } from 'auth/AuthProvider';
 const Providers: FC<PropsWithChildren> = ({ children }) => (
-  <HelmetProvider>
-    <ChakraProvider>
-      <Router>{children}</Router>
-    </ChakraProvider>
-  </HelmetProvider>
+  <AuthProvider>
+    <HelmetProvider>
+      <ChakraProvider>
+        <Router>{children}</Router>
+      </ChakraProvider>
+    </HelmetProvider>
+  </AuthProvider>
 );
 
 export default Providers;
---
+++ src/auth/AuthProvider.tsx
@@ -0,0 +1,49 @@
+import firebase from 'firebase/app';
+import { createContext, useEffect, useState, ReactNode } from 'react';
+import { auth } from 'Firebase';
+import { User } from 'firebase/auth';
+import Loading from 'components/organisms/Loading';
+
+type AuthContextProps = {
+  currentUser: User | null | undefined;
+  signInCheck: boolean;
+};
+
+const AuthContext = createContext<AuthContextProps>({
+  currentUser: undefined,
+  signInCheck: false,
+});
+
+const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const [currentUser, setCurrentUser] = useState<User | null | undefined>(
+    undefined
+  );
+
+  const [signInCheck, setSignInCheck] = useState(false);
+
+  // ログイン状態を確認する
+  useEffect(() => {
+    auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        setCurrentUser(user);
+        setSignInCheck(true);
+      } else {
+        setSignInCheck(true);
+      }
+    });
+  });
+
+  if (signInCheck) {
+    return (
+      <AuthContext.Provider value={{ currentUser, signInCheck }}>
+        {children}
+      </AuthContext.Provider>
+    );
+  } else {
+    // ログイン確認中
+    // 自分で作ったローディングコンポーネントをレンダリングする
+    return <Loading />;
+  }
+};
+
+export { AuthContext, AuthProvider };
