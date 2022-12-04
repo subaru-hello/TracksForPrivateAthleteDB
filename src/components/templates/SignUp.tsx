d065b9ea665fbf516733795ed1056b0610ad9832
subaru-hello <octosubaru0926@gmail.com>
2022-12-04 23:19:44 +0900

    add sign up page

--- src/components/templates/SignUp.tsx
+++ src/components/templates/SignUp.tsx
@@ -1,16 +1,38 @@
 import type { FC } from 'react';
+import { useContext, useState } from 'react';
+import { AuthContext } from 'auth/AuthProvider';
 import { auth } from 'Firebase';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { Link, useNavigate } from 'react-router-dom';
+import { createUserWithEmailAndPassword, User } from 'firebase/auth';
 import { Container, Box } from '@chakra-ui/react';
-
 export const SignUp: FC = () => {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const { email, password } = event.target.elements;
-    createUserWithEmailAndPassword(auth, email.value, password.value);
-  };
+  const [error, setError] = useState<string>('');
+  const [authenticating, setAuthenticating] = useState<boolean>(false);
+  const navigate = useNavigate();
 
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+    const target = event.target as typeof event.target & {
+      email: { value: string };
+      password: { value: string };
+    };
+    const email = target.email.value;
+    const password = target.password.value;
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        navigate('/');
+      })
+      .catch((error) => {
+        setAuthenticating(false);
+        setError(error.mesasage);
+      });
+  };
+  const { currentUser } = useContext(AuthContext);
+  if (currentUser) {
+    console.log(currentUser.uid);
+  }
   return (
+    // <AuthProvider>
     <Container>
       <Box>SignUp</Box>
       <form onSubmit={handleSubmit}>
@@ -23,10 +45,12 @@ export const SignUp: FC = () => {
           <input id="password" name="password" type="password" />
         </div>
         <div>
-          <button>登録</button>
+          <button disabled={authenticating}>新規登録</button>
         </div>
       </form>
+      <Link to="/login">既にアカウントをお持ちの方はこちら</Link>
     </Container>
+    // </AuthProvider>
   );
 };
 
