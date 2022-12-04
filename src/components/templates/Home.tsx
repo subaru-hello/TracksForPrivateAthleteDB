2a05ace340988483082a3e597be6d0932e892eb7
subaru-hello <octosubaru0926@gmail.com>
2022-12-04 23:21:25 +0900

    remove unused import

--- src/components/templates/Home.tsx
+++ src/components/templates/Home.tsx
@@ -1,27 +1,26 @@
 import type { FC } from 'react';
-import { Box, Heading, Container } from '@chakra-ui/react';
+import { Heading, Container } from '@chakra-ui/react';
 import { prefectureData } from 'data';
 import { Helmet } from 'react-helmet-async';
 import TrackIndex from 'components/organisms/TrackIndex';
 import SiteOutline from 'components/organisms/SiteOutline';
-import SignUp from 'components/templates/SignUp';
-
 const title = '競技場検索';
 
-const Home: FC = () => (
-  <Container centerContent={true}>
-    <Helmet>
-      <title>{title}</title>
-    </Helmet>
-    <Container>
-      <SignUp />
-      <Heading as="h1" size="xl" my={4}>
-        {title}
-      </Heading>
-      <SiteOutline my={10} />
-      <TrackIndex prefectures={prefectureData} />
+const Home: FC = () => {
+  return (
+    <Container centerContent={true}>
+      <Helmet>
+        <title>{title}</title>
+      </Helmet>
+      <Container>
+        <Heading as="h1" size="xl" my={4}>
+          {title}
+        </Heading>
+        <SiteOutline my={10} />
+        <TrackIndex prefectures={prefectureData} />
+      </Container>
     </Container>
-  </Container>
-);
+  );
+};
 
 export default Home;
