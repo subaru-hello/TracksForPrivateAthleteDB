771e32476ad07da8dc00cd27f6935a16a4767401
subaru-hello <octosubaru0926@gmail.com>
2022-12-04 23:17:05 +0900

    add loading page

---
+++ src/components/organisms/Loading.tsx
@@ -0,0 +1,6 @@
+import { Box, Container, Skeleton, Stack } from '@chakra-ui/react';
+const Loading = () => {
+  return <Container centerContent={true}>Loading中...</Container>;
+};
+
+export default Loading;
