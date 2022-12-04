793a86c6bebf9de5852cbb802ea0aa9b6837ed84
subaru-hello <octosubaru0926@gmail.com>
2022-12-05 08:49:03 +0900

    templates/layouts

--- src/components/templates/layouts/Header.tsx
+++ src/components/templates/layouts/Header.tsx
@@ -1,6 +1,6 @@
 import type { FC } from 'react';
 import { Box } from '@chakra-ui/react';
-import HeaderLinks from 'components/organisms/HeaderLinks';
+import HeaderLinks from 'components/organisms/layouts/HeaderLinks';
 const Header: FC = () => {
   return (
     <Box>
