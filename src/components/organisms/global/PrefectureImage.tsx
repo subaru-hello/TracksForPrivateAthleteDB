1d4eca375df4dde17a061657a45cb1fda060cbb0
subaru-hello <octosubaru0926@gmail.com>
2022-12-06 14:47:56 +0900

    add Prefecture Image

---
+++ src/components/organisms/global/PrefectureImage.tsx
@@ -0,0 +1,31 @@
+import type { FC } from 'react';
+import { Box } from '@chakra-ui/react';
+import { useState, useEffect } from 'react';
+import { storage } from 'Firebase';
+import { ref, getDownloadURL } from 'firebase/storage';
+import BaseImage from 'components/atoms/BaseImage';
+type Props = {
+  props: {
+    image: string;
+  };
+};
+const PrefectureImage: FC<Props> = ({ props }) => {
+  const [image, setImage] = useState('');
+  const gsReference = ref(storage, props.image);
+
+  useEffect(() => {
+    getDownloadURL(gsReference)
+      .then((url) => {
+        setImage(url);
+        console.log(image);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+  return (
+    <Box boxSize="sm">
+      <BaseImage props={{ image: image }} />
+    </Box>
+  );
+};
+
+export default PrefectureImage;
--- src/components/templates/layouts/Home.tsx
+++ src/components/templates/layouts/Home.tsx
@@ -1,9 +1,10 @@
 import type { FC } from 'react';
-import { Heading, Container } from '@chakra-ui/react';
+import { Heading, Container, Box } from '@chakra-ui/react';
 import { prefectureData } from 'data';
 import { Helmet } from 'react-helmet-async';
 import TrackIndex from 'components/organisms/tracks/TrackIndex';
 import SiteOutline from 'components/organisms/layouts/SiteOutline';
+import PrefectureImage from 'components/organisms/global/PrefectureImage';
 const title = '競技場検索';
 
 const Home: FC = () => {
@@ -12,13 +13,13 @@ const Home: FC = () => {
       <Helmet>
         <title>{title}</title>
       </Helmet>
-      <Container>
-        <Heading as="h1" size="xl" my={4}>
-          {title}
-        </Heading>
+      <Box>
         <SiteOutline my={10} />
         <TrackIndex prefectures={prefectureData} />
-      </Container>
+      </Box>
+      <Box>
+        <PrefectureImage props={{ image: '554.png' }} />
+      </Box>
     </Container>
   );
 };
