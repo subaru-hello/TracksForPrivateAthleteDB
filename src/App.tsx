14e61f784409cce6f6ff1d37f40ff9316c43ffcf
subaru-hello <octosubaru0926@gmail.com>
2022-12-07 12:02:54 +0900

    add footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import type { FC } from 'react';
 import Providers from 'Providers';
 import IndexRoutes from 'routes/index';
 import Header from 'components/templates/layouts/Header';
+import Footer from 'components/templates/layouts/Footer';
 
 const App: FC = () => (
   <Providers>
     <Header />
     <IndexRoutes />
+    <Footer />
   </Providers>
 );
 
---
+++ src/components/templates/layouts/Footer.tsx
@@ -0,0 +1,12 @@
+import type { FC } from 'react';
+import { Box } from '@chakra-ui/react';
+import FooterLinks from 'components/organisms/layouts/FooterLinks';
+const Footer: FC = () => {
+  return (
+    <Box>
+      <FooterLinks />
+    </Box>
+  );
+};
+
+export default Footer;
