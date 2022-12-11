9da6fe7e4b5e51e631fb192e25ae0601fbd8261b
subaru-hello <octosubaru0926@gmail.com>
2022-12-12 08:50:35 +0900

    add list key

--- src/components/organisms/tracks/TrackDetails.tsx
+++ src/components/organisms/tracks/TrackDetails.tsx
@@ -1,181 +1,204 @@
 import {
-    Box,
-    chakra,
-    Container,
-    Stack,
-    Text,
-    Image,
-    Flex,
-    VStack,
-    Button,
-    Heading,
-    SimpleGrid,
-    StackDivider,
-    useColorModeValue,
-    VisuallyHidden,
-    List,
-    ListItem,
-  } from '@chakra-ui/react';
-  import type { FC } from 'react';
-  import { useParams } from 'react-router-dom';
-  
-  import { trackData } from 'data';
-  import ForPublickDate from 'components/templates/tracks/ForPublicDate';
-  import HomeButton from 'components/organisms/layouts/HomeButton';
-  import { ExternalLinkIcon } from '@chakra-ui/icons';
-  import trackImage from 'assets/base_track.png';
-  
-  const TrackDetails = () => {
-    const { trackID = '' } = useParams();
-    const trackDetail = trackData.filter((track) => track.id === trackID);
-  
-    return (
-      <Box>
-        {trackDetail.map((track) => (
-          <SimpleGrid>
-            <Flex>
-              <Image
-                rounded={'md'}
-                alt={'product image'}
-                src={trackImage}
-                fit={'cover'}
-                align={'center'}
-                w={'100%'}
-                h={{ base: '100%', sm: '400px', lg: '500px' }}
-              />
-            </Flex>
-            <Stack spacing={{ base: 6, md: 10 }}>
-              <Box as={'header'}>
-                <Heading
-                  lineHeight={1.1}
-                  fontWeight={600}
-                  fontSize={{ base: '2xl', sm: '4xl', lg: '5xl' }}
-                >
-                  {track.name}
-                </Heading>
+  Box,
+  chakra,
+  Container,
+  Stack,
+  Text,
+  Image,
+  Flex,
+  VStack,
+  Button,
+  Heading,
+  SimpleGrid,
+  StackDivider,
+  useColorModeValue,
+  VisuallyHidden,
+  List,
+  ListItem,
+} from '@chakra-ui/react';
+import type { FC } from 'react';
+import { useParams } from 'react-router-dom';
+
+import { trackData } from 'data';
+import ForPublickDate from 'components/templates/tracks/ForPublicDate';
+import HomeButton from 'components/organisms/layouts/HomeButton';
+import { ExternalLinkIcon } from '@chakra-ui/icons';
+import trackImage from 'assets/base_track.png';
+
+const TrackDetails = () => {
+  const { trackID = '' } = useParams();
+  const trackDetail = trackData.filter((track) => track.id === trackID);
+
+  return (
+    <Box>
+      {trackDetail.map((track) => (
+        <SimpleGrid key={track.id}>
+          <Flex>
+            <Image
+              rounded={'md'}
+              alt={'product image'}
+              src={trackImage}
+              fit={'cover'}
+              align={'center'}
+              w={'100%'}
+              h={{ base: '100%', sm: '400px', lg: '500px' }}
+            />
+          </Flex>
+          <Stack spacing={{ base: 6, md: 10 }}>
+            <Box as={'header'}>
+              <Heading
+                lineHeight={1.1}
+                fontWeight={600}
+                fontSize={{ base: '2xl', sm: '4xl', lg: '5xl' }}
+              >
+                {track.name}
+              </Heading>
+              <Text
+                color={useColorModeValue('gray.900', 'gray.400')}
+                fontWeight={300}
+                fontSize={'2xl'}
+              >
+                神奈川県
+              </Text>
+            </Box>
+
+            <Stack
+              spacing={{ base: 4, sm: 6 }}
+              direction={'column'}
+              divider={
+                <StackDivider
+                  borderColor={useColorModeValue('gray.200', 'gray.600')}
+                />
+              }
+            >
+              <VStack spacing={{ base: 4, sm: 6 }}>
                 <Text
-                  color={useColorModeValue('gray.900', 'gray.400')}
-                  fontWeight={300}
+                  color={useColorModeValue('gray.500', 'gray.400')}
                   fontSize={'2xl'}
+                  fontWeight={'300'}
                 >
-                  神奈川県
+                  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                  diam nonumy eirmod tempor invidunt ut labore
                 </Text>
-              </Box>
-  
-              <Stack
-                spacing={{ base: 4, sm: 6 }}
-                direction={'column'}
-                divider={
-                  <StackDivider
-                    borderColor={useColorModeValue('gray.200', 'gray.600')}
-                  />
-                }
-              >
-                <VStack spacing={{ base: 4, sm: 6 }}>
-                  <Text
-                    color={useColorModeValue('gray.500', 'gray.400')}
-                    fontSize={'2xl'}
-                    fontWeight={'300'}
-                  >
-                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
-                    diam nonumy eirmod tempor invidunt ut labore
-                  </Text>
-                  <Text fontSize={'lg'}>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad
-                    aliquid amet at delectus doloribus dolorum expedita hic, ipsum
-                    maxime modi nam officiis porro, quae, quisquam quos
-                    reprehenderit velit? Natus, totam.
-                  </Text>
-                </VStack>
-                <Box>
-                  <Text
-                    fontSize={{ base: '16px', lg: '18px' }}
-                    color={useColorModeValue('yellow.500', 'yellow.300')}
-                    fontWeight={'500'}
-                    textTransform={'uppercase'}
-                    mb={'4'}
-                  >
-                    Features
-                  </Text>
-  
-                  <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10}>
-                    <List spacing={2}>
-                      <ListItem>ハードル可(木製)</ListItem>
-                      <ListItem>400mトラック</ListItem>{' '}
-                      <ListItem>高速タータン</ListItem>
-                    </List>
-                    <List spacing={2}>
-                      <ListItem>個人利用可</ListItem>
-                      <ListItem>砲丸・幅跳び可</ListItem>
-                      <ListItem>Small seconds</ListItem>
-                    </List>
-                  </SimpleGrid>
-                </Box>
-                <Box>
-                  <Text
-                    fontSize={{ base: '16px', lg: '18px' }}
-                    color={useColorModeValue('yellow.500', 'yellow.300')}
-                    fontWeight={'500'}
-                    textTransform={'uppercase'}
-                    mb={'4'}
-                  >
-                    詳細情報
-                  </Text>
-  
+                <Text fontSize={'lg'}>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad
+                  aliquid amet at delectus doloribus dolorum expedita hic, ipsum
+                  maxime modi nam officiis porro, quae, quisquam quos
+                  reprehenderit velit? Natus, totam.
+                </Text>
+              </VStack>
+              <Box>
+                <Text
+                  fontSize={{ base: '16px', lg: '18px' }}
+                  color={useColorModeValue('yellow.500', 'yellow.300')}
+                  fontWeight={'500'}
+                  textTransform={'uppercase'}
+                  mb={'4'}
+                >
+                  Features
+                </Text>
+
+                <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10}>
                   <List spacing={2}>
-                    <ListItem>
-                      <Text as={'span'} fontWeight={'bold'}>
-                        場所:
-                      </Text>{' '}
-                      {track.address}
-                    </ListItem>
-                    <ListItem>
-                      <Text as={'span'} fontWeight={'bold'}>
-                        個人解放時間:
-                      </Text>{' '}
-                      {track.open_hour}
-                    </ListItem>
-                    <ListItem>
-                      <Text as={'span'} fontWeight={'bold'}>
-                        利用料金
-                      </Text>{' '}
-                      {track.entrance_fee}
-                    </ListItem>
-                    <ListItem>
-                      <Text as={'span'} fontWeight={'bold'}>
-                        サイトURL:
-                      </Text>{' '}
-                      <a href={track.site_url} target="_blank" rel="noreferrer">
-                        サイトURL: {track.site_url}
-                        <ExternalLinkIcon mx="2px" />
-                      </a>
-                    </ListItem>
+                    <ListItem>ハードル可(木製)</ListItem>
+                    <ListItem>400mトラック</ListItem>{' '}
+                    <ListItem>高速タータン</ListItem>
                   </List>
-                </Box>
-              </Stack>
-  
-              <Button
-                rounded={'none'}
-                w={'full'}
-                mt={8}
-                size={'lg'}
-                py={'7'}
-                bg={useColorModeValue('gray.900', 'gray.50')}
-                color={useColorModeValue('white', 'gray.900')}
-                textTransform={'uppercase'}
-                _hover={{
-                  transform: 'translateY(2px)',
-                  boxShadow: 'lg',
-                }}
-              >
-                お気に入りに登録
-              </Button>
+                  <List spacing={2}>
+                    <ListItem>個人利用可</ListItem>
+                    <ListItem>砲丸・幅跳び可</ListItem>
+                    <ListItem>Small seconds</ListItem>
+                  </List>
+                </SimpleGrid>
+              </Box>
+              <Box>
+                <Text
+                  fontSize={{ base: '16px', lg: '18px' }}
+                  color={useColorModeValue('yellow.500', 'yellow.300')}
+                  fontWeight={'500'}
+                  textTransform={'uppercase'}
+                  mb={'4'}
+                >
+                  今日の空き状況
+                </Text>
+
+                <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10}>
+                  <List spacing={2}>
+                    <ListItem>ハードル可(木製)</ListItem>
+                    <ListItem>400mトラック</ListItem>{' '}
+                    <ListItem>高速タータン</ListItem>
+                  </List>
+                  <List spacing={2}>
+                    <ListItem>個人利用可</ListItem>
+                    <ListItem>砲丸・幅跳び可</ListItem>
+                    <ListItem>Small seconds</ListItem>
+                  </List>
+                </SimpleGrid>
+              </Box>
+              <Box>
+                <Text
+                  fontSize={{ base: '16px', lg: '18px' }}
+                  color={useColorModeValue('yellow.500', 'yellow.300')}
+                  fontWeight={'500'}
+                  textTransform={'uppercase'}
+                  mb={'4'}
+                >
+                  詳細情報
+                </Text>
+
+                <List spacing={2}>
+                  <ListItem>
+                    <Text as={'span'} fontWeight={'bold'}>
+                      場所:
+                    </Text>{' '}
+                    {track.address}
+                  </ListItem>
+                  <ListItem>
+                    <Text as={'span'} fontWeight={'bold'}>
+                      個人解放時間:
+                    </Text>{' '}
+                    {track.open_hour}
+                  </ListItem>
+                  <ListItem>
+                    <Text as={'span'} fontWeight={'bold'}>
+                      利用料金
+                    </Text>{' '}
+                    {track.entrance_fee}
+                  </ListItem>
+                  <ListItem>
+                    <Text as={'span'} fontWeight={'bold'}>
+                      サイトURL:
+                    </Text>{' '}
+                    <a href={track.site_url} target="_blank" rel="noreferrer">
+                      サイトURL: {track.site_url}
+                      <ExternalLinkIcon mx="2px" />
+                    </a>
+                  </ListItem>
+                </List>
+              </Box>
             </Stack>
-          </SimpleGrid>
-        ))}
-      </Box>
-    );
-  };
-  
-  export default TrackDetails;
-  
+
+            <Button
+              rounded={'none'}
+              w={'full'}
+              mt={8}
+              size={'lg'}
+              py={'7'}
+              bg={useColorModeValue('gray.900', 'gray.50')}
+              color={useColorModeValue('white', 'gray.900')}
+              textTransform={'uppercase'}
+              _hover={{
+                transform: 'translateY(2px)',
+                boxShadow: 'lg',
+              }}
+            >
+              お気に入りに登録
+            </Button>
+          </Stack>
+        </SimpleGrid>
+      ))}
+    </Box>
+  );
+};
+
+export default TrackDetails;
